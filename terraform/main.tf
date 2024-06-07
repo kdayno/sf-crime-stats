54f48fd6260869ab1e6ad65cb6bcdba280f6c5c0
Kevin Dayno <57159543+kdayno@users.noreply.github.com>
2024-06-07 21:14:09 +0000

    Create initial terraform config

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.32.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "./keys/keys.json"
+  project     = "sf-crime-stats"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "sf-crime-data" {
+  name          = "sf-crime-stats-terra-bucket"
+  location      = "US"
+  force_destroy = true
+  storage_class = "STANDARD"
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}

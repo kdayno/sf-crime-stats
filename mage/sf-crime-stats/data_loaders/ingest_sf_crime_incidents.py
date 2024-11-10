0f177f1ac68a7b7cd2481a6402dae1c1bbb55569
Kevin Dayno <57159543+kdayno@users.noreply.github.com>
2024-11-10 18:20:13 +0000

    Update daily ingestion pipelines - Add input_date param - Update logic to handle various input_date scenarios

--- mage/sf-crime-stats/data_loaders/ingest_sf_crime_incidents.py
+++ mage/sf-crime-stats/data_loaders/ingest_sf_crime_incidents.py
@@ -18,10 +18,38 @@ def load_data_from_api(*args, **kwargs):
 
     est = pytz.timezone('US/Eastern')
   
-    previous_date = (dt.datetime.now(est) - dt.timedelta(kwargs['days_offset'])).strftime('%Y-%m-%dT00:00:00.000')
-    print(previous_date)
+    input_date = kwargs['input_date']
+    days_offset = kwargs['days_offset']
+    execution_date = kwargs['execution_date']
 
-    url = f"https://data.sfgov.org/resource/wg3w-h783.json?incident_date={previous_date}"
+    # If no input_date is provided, set input_date as current date - 1 day
+    # The data for the current date will only be available the next day. Data is always lagging by 1 day
+    # It's also possible that yesterday's data is not immmediately available since the API is refreshed daily at 10 AM PST
+    if input_date == 'YYYY-MM-DD':
+        try:
+            input_date = (execution_date - dt.timedelta(1))
+
+        except ValueError as e:
+            print("Input date INVALID. Provide date in the format 'YYYY-MM-DD'.", end='\n')
+            raise e
+
+    else:
+        try:
+            input_date = dt.datetime.strptime(input_date, '%Y-%m-%d')
+
+        except ValueError as e:
+                print("Input date INVALID. Provide date in the format 'YYYY-MM-DD'.", end='\n')
+                raise e
+
+    # Determine data from which DATE to load accounting for specified offset
+    if days_offset > 0:
+        date_of_data_to_load = (input_date - dt.timedelta(days_offset)).strftime('%Y-%m-%dT00:00:00.000')
+
+    else:
+        date_of_data_to_load = input_date.strftime('%Y-%m-%dT00:00:00.000')
+
+
+    url = f"https://data.sfgov.org/resource/wg3w-h783.json?incident_date={date_of_data_to_load}"
 
     r = requests.get(url)
 
@@ -34,9 +62,9 @@ def load_data_from_api(*args, **kwargs):
     return df
 
 
-@test
-def test_output(output, *args) -> None:
-    """
-    Template code for testing the output of the block.
-    """
-    assert output is not None, 'The output is undefined'
+# @test
+# def test_output(output, *args) -> None:
+#     """
+#     Template code for testing the output of the block.
+#     """
+#     assert output is not None, 'The output is undefined'

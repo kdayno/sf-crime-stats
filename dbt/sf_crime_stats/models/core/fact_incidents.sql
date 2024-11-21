a5095ac8c01efc1e41603aaad66294de19e29b3e
Kev D <kdayno.documents@gmail.com>
2024-11-21 02:27:21 +0000

    Fix join condition

--- dbt/sf_crime_stats/models/core/fact_incidents.sql
+++ dbt/sf_crime_stats/models/core/fact_incidents.sql
@@ -31,7 +31,7 @@ with
         from subset fct_i
         inner join dim_incident_details dim_id on  fct_i.incident_details_fk = dim_id.incident_details_id
         inner join dim_report_details dim_rd on  fct_i.report_details_fk = dim_rd.report_details_id
-        inner join dim_incident_date dim_idate on  fct_i.incident_date = dim_idate.incident_date_fk
+        inner join dim_incident_date dim_idate on  fct_i.incident_date_fk = dim_idate.incident_date
         inner join dim_report_date dim_rdate on  fct_i.report_date_fk = dim_rdate.report_date
         inner join dim_location dim_l on  fct_i.location_fk = dim_l.location_id
     )

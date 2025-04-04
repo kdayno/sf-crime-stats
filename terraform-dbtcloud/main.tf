11f4128594d2d50f72c2df00b62dec5414e4ff3f
Kevin Dayno <57159543+kdayno@users.noreply.github.com>
2025-04-04 22:54:23 +0000

    Update dbt cloud provider version

--- terraform-dbtcloud/main.tf
+++ terraform-dbtcloud/main.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     dbtcloud = {
       source  = "dbt-labs/dbtcloud"
-      version = "0.3.13"
+      version = "0.3.26"
     }
   }
 }
-
 provider "dbtcloud" {
   account_id = var.dbt_account_id
   token      = var.dbt_token

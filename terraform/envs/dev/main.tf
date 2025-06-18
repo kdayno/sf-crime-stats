4d30ae258fe8a790de933bbd1139709937f4fbf2
Kevin Dayno <57159543+kdayno@users.noreply.github.com>
2025-06-18 03:26:53 +0000

    Integrate cloud run tf scripts into dev env

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -5,21 +5,30 @@ module "gcp" {
     project_name = var.project_name
     location = var.location
     region = var.region
+    zone = var.zone
+    app_name = var.app_name
+    docker_image = var.docker_image
+    container_cpu = var.container_cpu
+    container_memory = var.container_memory
+    database_user = var.database_user
+    database_password = var.database_password
+    domain = var.domain
+    ssl = var.ssl
 }
 
-module "dbtcloud" {
-    source = "../../modules/dbtcloud"
-    env = var.env
-    dbt_account_id = var.dbt_account_id
-    dbt_token = var.dbt_token
-    dbt_host_url = var.dbt_host_url
-    gcp_project_id = var.gcp_project_id
-    private_key_id = var.private_key_id
-    private_key = var.private_key
-    client_email = var.client_email
-    client_id = var.client_id
-    auth_uri = var.auth_uri
-    token_uri = var.token_uri
-    auth_provider_x509_cert_url = var.auth_provider_x509_cert_url
-    client_x509_cert_url = var.client_x509_cert_url
-}
+# module "dbtcloud" {
+#     source = "../../modules/dbtcloud"
+#     env = var.env
+#     dbt_account_id = var.dbt_account_id
+#     dbt_token = var.dbt_token
+#     dbt_host_url = var.dbt_host_url
+#     gcp_project_id = var.gcp_project_id
+#     private_key_id = var.private_key_id
+#     private_key = var.private_key
+#     client_email = var.client_email
+#     client_id = var.client_id
+#     auth_uri = var.auth_uri
+#     token_uri = var.token_uri
+#     auth_provider_x509_cert_url = var.auth_provider_x509_cert_url
+#     client_x509_cert_url = var.client_x509_cert_url
+# }
--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -26,6 +26,46 @@ variable "region" {
   default     = "us-west1"
 }
 
+variable "zone" {
+  description = "Project Zone"
+  type        = string
+  default     = "us-west1-a"
+}
+
+variable "app_name" {
+  description = "Application Name"
+  type        = string
+  default     = "sf-crime-stats"
+}
+
+variable "docker_image" {
+  description = "The docker image to deploy to Cloud Run."
+  type        = string
+  default     = "kdayno/sf-crime-stats:latest"
+}
+variable "container_cpu" {
+  description = "Container cpu"
+  type        = string
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  type        = string
+  default     = "2G"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "mageuser"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+}
 
 ### dbt Cloud specific variables ###
 # All GCP values can be obtained from the credentials .json file that is generated when creating a service account on GCP
@@ -76,4 +116,16 @@ variable "auth_provider_x509_cert_url" {
 
 variable "client_x509_cert_url" {
   type = string
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
 }
